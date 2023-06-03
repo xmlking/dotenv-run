0c821871a6643854e0aeaf69a17dbd1410d05c45
Chihab Otmani <chihab@gmail.com>
2023-06-03 16:57:12 +0100

    chore: init @dotenv-run monorepo 🚀

---
+++ packages/cli/src/index.ts
@@ -0,0 +1,31 @@
+import { paths, expand } from "@dotenv-run/core";
+import * as chalk from "chalk";
+import * as spawn from "cross-spawn";
+import * as minimist from "minimist";
+
+const argv = minimist(process.argv.slice(2));
+
+const root = argv.e;
+const verbose = argv.v;
+
+const envPaths = paths(process.env.NODE_ENV, root);
+if (verbose) {
+  envPaths.forEach((envPath) => {
+    console.log(`${chalk.green(" ✔")} ${envPath}`);
+  });
+}
+expand(envPaths);
+
+const command = argv._[0];
+if (command) {
+  spawn(command, argv._.slice(1), { stdio: "inherit" }).on(
+    "exit",
+    function (exitCode, signal) {
+      if (typeof exitCode === "number") {
+        process.exit(exitCode);
+      } else {
+        process.kill(process.pid, signal);
+      }
+    }
+  );
+}
---
+++ packages/core/src/index.ts
@@ -0,0 +1,77 @@
+import { config } from "dotenv";
+import { expand as dotenvExpand } from "dotenv-expand";
+import * as fs from "fs";
+import * as path from "path";
+
+export type Env = Record<string, string>;
+
+function isSubfolder(parent: string, child: string) {
+  return path.relative(parent, child).startsWith("..");
+}
+
+function getAbsoluteEnvPath(envPath: string) {
+  const _envPath = path.isAbsolute(envPath)
+    ? envPath
+    : path.resolve(process.cwd(), envPath);
+  return fs.existsSync(_envPath)
+    ? fs.lstatSync(_envPath).isDirectory()
+      ? _envPath
+      : path.dirname(_envPath)
+    : process.cwd();
+}
+
+function getPathsDownTo(envPath: string, destination: string) {
+  let currentPath = destination;
+  const paths = [currentPath];
+  while (currentPath !== envPath && currentPath !== "/") {
+    currentPath = path.dirname(currentPath);
+    paths.push(currentPath);
+  }
+  return paths;
+}
+
+function buildEnvFiles(environment: string, envPath: string) {
+  return [
+    environment !== "test" && `${envPath}.${environment}.local`, // .env.development.local, .env.test.local, .env.production.local
+    // Don't include `.env.local` for `test` environment
+    // since normally you expect tests to produce the same
+    // results for everyone
+    environment && `${envPath}.${environment}`, // .env.development, .env.test, .env.production
+    environment !== "test" && `${envPath}.local`, // .env.local
+    envPath, // .env
+  ].filter(Boolean);
+}
+
+export function paths(environment: string, root: string) {
+  const _root = getAbsoluteEnvPath(root); // resolved path to .env file
+  let envPaths: string[] = [];
+  if (isSubfolder(_root, process.cwd())) {
+    envPaths = [_root];
+  } else {
+    envPaths = getPathsDownTo(_root, process.cwd());
+  }
+  return envPaths
+    .map((envPath) => path.join(envPath, ".env"))
+    .flatMap((envPath) => buildEnvFiles(environment, envPath))
+    .filter((envPath) => fs.existsSync(envPath));
+}
+
+export function expand(envPaths: string[]) {
+  envPaths.forEach((dotenvFile) => {
+    dotenvExpand(
+      config({
+        path: dotenvFile,
+      })
+    );
+  });
+}
+
+export function filter(env: Env, prefix: RegExp): Env {
+  return Object.keys(env)
+    .filter((key) => prefix.test(key))
+    .sort() // sort keys to make output more deterministic
+    .reduce<Env>((env, key) => {
+      env[key] = process.env[key];
+      return env;
+    }, {});
+}
